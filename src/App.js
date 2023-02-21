0468f703b521a15371274548aa29e2c50a65da98
robby-o <robby.olson11@gmail.com>
2023-02-20 19:10:11 -0800

    migrated to redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
 
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
-} from './utils/firebase/firebase.utils';
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentication/authentication.component';
-import Shop from './routes/shop/shop.component';
-import Checkout from './routes/checkout/checkout.component';
-import { setCurrentUser } from './store/user/user.action';
+} from './utils/firebase/firebase.utils'
+import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component'
+import Authentication from './routes/authentication/authentication.component'
+import Shop from './routes/shop/shop.component'
+import Checkout from './routes/checkout/checkout.component'
+import { setCurrentUser } from './store/user/user.reducer'
 
 const App = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
-        createUserDocumentFromAuth(user);
+        createUserDocumentFromAuth(user)
       }
-      dispatch(setCurrentUser(user));
-    });
+      const pickedUser =
+        user &&
+        (({ accessToken, email }) =>
+          ({
+            accessToken,
+            email,
+          }(user)))
+      dispatch(setCurrentUser(pickedUser))
+    })
 
-    return unsubscribe;
-  }, []);
+    return unsubscribe
+  }, [])
 
   return (
     <Routes>
@@ -37,7 +44,7 @@ const App = () => {
         <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,27 +1,27 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux'
 
 import {
   selectCartCount,
   selectIsCartOpen,
-} from '../../store/cart/cart.selector';
-import { setIsCartOpen } from '../../store/cart/cart.action';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+} from '../../store/cart/cart.selector'
+import { setIsCartOpen } from '../../store/cart/cart.reducer'
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
-import { CartIconContainer, ItemCount } from './cart-icon.styles';
+import { CartIconContainer, ItemCount } from './cart-icon.styles'
 
 const CartIcon = () => {
-  const dispatch = useDispatch();
-  const isCartOpen = useSelector(selectIsCartOpen);
-  const cartCount = useSelector(selectCartCount);
+  const dispatch = useDispatch()
+  const isCartOpen = useSelector(selectIsCartOpen)
+  const cartCount = useSelector(selectCartCount)
 
-  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen))
 
   return (
     <CartIconContainer onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
       <ItemCount>{cartCount}</ItemCount>
     </CartIconContainer>
-  );
-};
+  )
+}
 
-export default CartIcon;
+export default CartIcon
